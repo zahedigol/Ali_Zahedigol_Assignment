e1ed4f295ce6f126bd11aaba494b27df59787af3
Ali Zahedigol <ali.zahedigol@gmail.com>
2022-07-07 16:25:13 +0300

    sales import csv read from binary file. usage for upload in memory

--- sales/utils.py
+++ sales/utils.py
@@ -1,3 +1,4 @@
+import codecs
 import csv
 import decimal
 from pathlib import Path
@@ -9,26 +10,37 @@ def normalize_product_name(product_name: str):
     return product_name.lower().replace(' ', '_')
 
 
-def import_sales_from_csv(model, fixture_file: Path, user_id: int):
-    with open(fixture_file, mode='r') as read_obj:
-        sniffer = csv.Sniffer()
-        dialect = sniffer.sniff(read_obj.readline())
-        read_obj.seek(0)
-        csv_reader = csv.DictReader(
-            read_obj,
-            delimiter=dialect.delimiter,
-        )
-        for row in csv_reader:
-            if not all(field.strip() for field in row):
-                continue
-            date = parse_date(row['date'])
-            pre_condition = {
-                'user_id': user_id,
-                'date': date,
-                'product_normalized': normalize_product_name(row['product']),
-            }
-            sale, *_ = model.objects.get_or_create(**pre_condition)
-            sale.product = row['product']
-            sale.revenue += decimal.Decimal(row['revenue'])
-            sale.sales_number += int(row['sales_number'])
-            sale.save()
+def import_sales_from_csv(model, user_id: int, fixture_file: Path = None, reader=None):
+    if not fixture_file and not reader:
+        raise ValueError('Either fixture_file or reader must be provided')
+    if fixture_file and reader:
+        raise ValueError('Only one of fixture_file or reader must be provided')
+
+    if reader:
+        return _import_with_reader(model, reader, user_id)
+    with open(fixture_file, mode='rb') as reader:
+        return _import_with_reader(model, reader, user_id)
+
+
+def _import_with_reader(model, reader, user_id: int):
+    sniffer = csv.Sniffer()
+    dialect = sniffer.sniff(reader.readline().decode('utf-8'))
+    reader.seek(0)
+    csv_reader = csv.DictReader(
+        codecs.iterdecode(reader, 'utf-8'),
+        delimiter=dialect.delimiter,
+    )
+    for row in csv_reader:
+        if not all(field.strip() for field in row):
+            continue
+        date = parse_date(row['date'])
+        pre_condition = {
+            'user_id': user_id,
+            'date': date,
+            'product_normalized': normalize_product_name(row['product']),
+        }
+        sale, *_ = model.objects.get_or_create(**pre_condition)
+        sale.product = row['product']
+        sale.revenue += decimal.Decimal(row['revenue'])
+        sale.sales_number += int(row['sales_number'])
+        sale.save()
